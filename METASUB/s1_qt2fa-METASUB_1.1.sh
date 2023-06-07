2b1fcd076e7919cf5861bae1a04279e9c2931670
Ryo Honda <ryohonda@MacBook-Air-M2---RHonda.local>
2023-06-07 19:43:23 +0930

    added lines for importing a file list

--- METASUB/s1_qt2fa-METASUB_1.1.sh
+++ METASUB/s1_qt2fa-METASUB_1.1.sh
@@ -6,7 +6,7 @@
 #$ -pe def_slot 8
 
 #==============================================================================
-# qt2fa-AMR, METASUB version / created by Ryo Honda (END-AMR-Asia), 2023-05-27 
+# qt2fa-AMR, METASUB version / created by Ryo Honda (END-AMR-Asia), 2023-06-07 
 # The shell script for NIG supercomputer (Grid Engine) 
 # with Singularity package of BioContainer
 #==============================================================================
@@ -20,11 +20,26 @@
 export SINGULARITY_BINDPATH="/home/user/METASUB"
 
 ####### Parameter setting ######################################################
-# Sample name prefix of sequence files, 配列ファイル名の最初についているサンプル名
-SAMPLE="DNBSEQ_"
-# Numbering of samples to be processed 処理する配列ファイルの番号
-START=270
-END=270
+# Your working directory 自分の作業ディレクトリ
+DIR_WORKING="/home/user/METASUB"
+
+## Listing sequence data files 
+## Case 1: to input the list from a text file (1 file in 1 line).
+## 配列リストをファイルから読み込む（1行1ファイルで記述）
+## 【重要】改行コードはLF(UNIX/Mac)で。Windows標準ではCR+LFになってしまうので，エディタでLF(UNIX/Mac)を必ず指定。
+#FILE_LIST="${DIR_WORKING}/seqlist.txt"
+#IFS=$'\n'  # CR+LF(Windows)の場合は '\r\n' にする
+#LIST=(`cat ${FILE_LIST}`)
+
+## Case 2: for sequential numbered files 連番のついたファイルの場合
+## Sample name prefix of sequence files, 配列ファイル名の最初についているサンプル名
+PREFIX="DNBSEQ_"
+## Numbering of samples to be processed 処理する配列ファイルの番号
+START=100
+END=101
+for ((i=0;i<=`expr $END - $START`;i++)); do
+	n=`expr $i + $START`; LIST[$i]="${PREFIX}${n}"
+done
 
 # The directory of raw sequence files (specify the absolute path. do not include the final '/') 
 # 配列生データのあるディレクトリ（絶対パスで指定。最後にスラッシュ '/' は含めない）
@@ -32,9 +47,9 @@ DIR_RAW="/home/user/METASUB/0.raw"
 
 # The directories for output files (specify the absolute path. do not include the final '/')  
 # 配列ファイルを出力するディレクトリ（絶対パスで指定。最後にスラッシュ '/' は含めない）
-DIR_OUT="/home/user/METASUB"
-DIR_QT="${DIR_OUT}/1.trimmed"
-DIR_FA="${DIR_OUT}/2.fasta"
+DIR_WORKING="/home/user/METASUB"
+DIR_QT="${DIR_WORKING}/1.trimmed"
+DIR_FA="${DIR_WORKING}/2.fasta"
 
 # location of the container image for singularity 
 FASTP="/usr/local/biotools/f/fastp:0.23.3--h5f740d0_0"
@@ -44,36 +59,37 @@ FASTP="/usr/local/biotools/f/fastp:0.23.3--h5f740d0_0"
 # Check the file names in the command line below are correct. 
 
 DATE=$(date '+%Y-%m-%d %H:%M:%S %z')
-echo "[${DATE}] $JOB_NAME started. ${SAMPLE}${START}-${END}"
+i=0; n=${#LIST[@]}
+echo "[${DATE}] $JOB_NAME started. (${i}/${n})"
 # make output directories if not existed
 mkdir -p ${DIR_QT} ${DIR_FA}
 dir_tmp="${DIR_FA}/tmp"; mkdir -p ${dir_tmp}; trap 'rm -R ${dir_tmp}' 0
 
-for ((i=START; i<=END; i++))
-do
+for SAMPLE in "${LIST[@]}"; do
 	# quality trimming by fastp
 	singularity exec ${FASTP} fastp\
-	--in1 ${DIR_RAW}/${SAMPLE}${i}_Read1.fq.gz\
-	--in2 ${DIR_RAW}/${SAMPLE}${i}_Read2.fq.gz\
-	--out1 ${DIR_QT}/${SAMPLE}${i}_Read1.qt.fq.gz\
-	--out2 ${DIR_QT}/${SAMPLE}${i}_Read2.qt.fq.gz\
-	--html ${DIR_QT}/${SAMPLE}${i}.report.html\
-	--json ${DIR_QT}/${SAMPLE}${i}.report.json\
+	--in1 ${DIR_RAW}/${SAMPLE}_Read1.fq.gz\
+	--in2 ${DIR_RAW}/${SAMPLE}_Read2.fq.gz\
+	--out1 ${DIR_QT}/${SAMPLE}_Read1.qt.fq.gz\
+	--out2 ${DIR_QT}/${SAMPLE}_Read2.qt.fq.gz\
+	--html ${DIR_QT}/${SAMPLE}.report.html\
+	--json ${DIR_QT}/${SAMPLE}.report.json\
 	-q 20 -t 1 -T 1 -l 20 -w 8
 	
 	# convert fastq to fasta
-	gunzip -c ${DIR_QT}/${SAMPLE}${i}_Read1.qt.fq.gz | awk '(NR - 1) % 4 < 2' | sed 's/@/>/' > ${dir_tmp}/${SAMPLE}${i}_R1.fa
-	gunzip -c ${DIR_QT}/${SAMPLE}${i}_Read2.qt.fq.gz | awk '(NR - 1) % 4 < 2' | sed 's/@/>/' > ${dir_tmp}/${SAMPLE}${i}_R2.fa
+	gunzip -c ${DIR_QT}/${SAMPLE}_Read1.qt.fq.gz | awk '(NR - 1) % 4 < 2' | sed 's/@/>/' > ${dir_tmp}/${SAMPLE}_R1.fa
+	gunzip -c ${DIR_QT}/${SAMPLE}_Read2.qt.fq.gz | awk '(NR - 1) % 4 < 2' | sed 's/@/>/' > ${dir_tmp}/${SAMPLE}_R2.fa
 	# interpose two fasta files (R1&R2) into one fasta file (R12).
-	cat ${dir_tmp}/${SAMPLE}${i}_R1.fa ${dir_tmp}/${SAMPLE}${i}_R2.fa > ${DIR_FA}/${SAMPLE}${i}_R12.fa
-	rm ${dir_tmp}/${SAMPLE}${i}_R1.fa ${dir_tmp}/${SAMPLE}${i}_R2.fa
+	cat ${dir_tmp}/${SAMPLE}_R1.fa ${dir_tmp}/${SAMPLE}_R2.fa > ${DIR_FA}/${SAMPLE}_R12.fa
+	rm ${dir_tmp}/${SAMPLE}_R1.fa ${dir_tmp}/${SAMPLE}_R2.fa
 	
 	# record the progress
 	DATE=$(date '+%Y-%m-%d %H:%M:%S %z')
-	echo "[${DATE}] ${SAMPLE}${i} is done."
+	i=`expr $i + 1`
+	echo "[${DATE}] ${SAMPLE} is done. (${i}/${n})"
 done
 # record the completion of the script
 DATE=$(date '+%Y-%m-%d %H:%M:%S %z')
-echo "[$DATE] $JOB_NAME completed."
+echo "[$DATE] $JOB_NAME completed. (${i}/${n})"
 exit 0
 
