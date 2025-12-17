79570ed4f7f0fd21cf7b447e477ce52f1c3ec4ba
Ryo Honda <rhonda@iMac-RHonda.local>
2025-12-17 16:57:35 +0900

    minor updates

--- 16S_taxonomy/js1_dada2.sh
+++ 16S_taxonomy/js1_dada2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --output=/home/ryohonda/log/%x_%j.out.log    # ★ユーザー名変更
-#SBATCH --error=/home/ryohonda/log/%x_%j.err.log     # ★ユーザー名変更
+#SBATCH --output=/home/username/log/%x_%j.out.log    # ★ユーザー名変更
+#SBATCH --error=/home/username/log/%x_%j.err.log     # ★ユーザー名変更
 #SBATCH -p medium
 #SBATCH -N 1-1
 #SBATCH -n 8
-#SBATCH --mem=32G      # 45サンプルなら十分(必要に応じて変更)
+#SBATCH --mem=32G      # 100サンプル超えないなら十分(必要に応じて変更)
 
 # スクリプト実行（★パス変更）
-Rscript /home/ryohonda/Sakai/s1_dada2_pipeline.r
+Rscript /home/username/16S/SILVA/scripts/s1_dada2_pipeline.r
